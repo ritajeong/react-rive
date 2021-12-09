33414995a93bf7f6850664a5bca22b05cf74649b
ritajeong <litch0s2@gmail.com>
2021-12-10 06:32:29 +0900

    feat: add stateMachine, change hover to onclick

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
-import { useRive } from "rive-react";
+import { useRive, useStateMachineInput } from "rive-react";
 
 function App() {
+  const STATE_MACHINE_NAME = "Basic State Machine";
+  const INPUT_NAME = "Switch";
+
   const { rive, RiveComponent } = useRive({
     src: "hamburger-time.riv",
-    animations: "Hamburger to X",
-    autoplay: false,
+    stateMachines:STATE_MACHINE_NAME,
+    autoplay: true,
   });
+  
+  //state machine check 
+  // if (rive) {
+  //   console.log(rive.contents);
+  // }
+  
+  const onClickInput = useStateMachineInput(
+    rive,
+    STATE_MACHINE_NAME,
+    INPUT_NAME
+  )
 
   return (
     <div>
-      <RiveComponent onMouseOver={() => rive.play()} />
+      <RiveComponent onClick={()=> onClickInput.fire()}/>
     </div>
   );
 }
