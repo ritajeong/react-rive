ee50d05cf3be4ddd62e0bd6f640bd89ec97eb1b8
ritajeong <litch0s2@gmail.com>
2021-12-10 08:38:12 +0900

    feat: resize Hamburgur

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ function App() {
 
   return (
     <div>
-      <RiveHamburger/>
+      <RiveHamburger className="Big" />
+      <RiveHamburger className="Small"/>
     </div>
   );
 }
--- src/RiveHamburger.js
+++ src/RiveHamburger.js
@@ -1,6 +1,6 @@
 import { useRive, useStateMachineInput } from "rive-react";
-
-export default function RiveHamburger() {
+import "./RiveHamburger.css"
+export default function RiveHamburger({className}) {
   const STATE_MACHINE_NAME = "Basic State Machine";
   const INPUT_NAME = "Switch";
 
@@ -16,6 +16,6 @@ export default function RiveHamburger() {
     INPUT_NAME
   )
 
-  return <RiveComponent onClick={()=> onClickInput.fire()}/>;
+  return <RiveComponent className={className} onClick={()=> onClickInput.fire()}/>;
 
 }
